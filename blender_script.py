4b10b94061570df4cc65a31cd5d510b21c41a1bc
Greg Kasapidis <gregkwaste@gmail.com>
2016-11-12 02:20:05 +0200

    First version of blender script

--- blender_script.py
+++ blender_script.py
@@ -1,48 +1,113 @@
 import bpy
 import bmesh
+import os
+import sys
+from math import radians
+from mathutils import Matrix,Vector
+
+# Add script path to sys.path
+scriptpath = bpy.context.space_data.text.filepath
+dir = os.path.dirname(scriptpath)
+print(dir)
+
+if not dir in sys.path:
+    sys.path.append(dir )
+    #print(sys.path)
 
 from main import Create_Data
 
-obj = bpy.context.scene.objects.active
-bpy.ops.object.mode_set(mode='EDIT')
-bpy.ops.mesh.quads_convert_to_tris()
-mesh = obj.data
-bm = bmesh.from_edit_mesh(mesh)
 
-def uv_from_vert_first(uv_layer, v):
-    for l in v.link_loops:
-        uv_data = l[uv_layer]
-        return uv_data.uv
-    return None
+scn = bpy.context.scene
 
-name = obj.name
-#print('name: {}'.format(obj.name))
+icounter = 0
+vcounter = 0
+vertices = []
+normals  = [] 
+indices  = []
+uvs      = []
+tangents = []
 
-vertices = list((v.co.x, v.co.y, v.co.z, 1) for v in mesh.vertices)
+objects  = []
 
-indexes = list()
-uvs = list()
-for poly in mesh.polygons:
-    indexes.append(tuple(mesh.loops[i].vertex_index for i in range(poly.loop_start, poly.loop_start + poly.loop_total)))
 
-uv_layer = bm.loops.layers.uv.active
-
-uvs = list()
-
-for v in bm.verts:
-    uv_vect = uv_from_vert_first(uv_layer, v)
-    uvs.append((uv_vect[0], uv_vect[1], 0, 1))
+for ob in scn.objects:
+    if not ob.name.startswith('NMS'):
+        continue
     
+    print('Located Object for export', ob.name)
+    objects.append(ob.name.upper())
+    bm = bmesh.new()
+    bm.from_mesh(ob.data)
+    
+    # Matrices
+    object_matrix_wrld = ob.matrix_world
+    rot_x_mat = Matrix.Rotation(radians(-90), 4, 'X')
+    scale_mat = Matrix.Scale(1, 4)
+    norm_mat = ob.matrix_world.inverted().transposed()
+    
+    uvcount = len(bm.loops.layers.uv)
+    colcount = len(bm.loops.layers.color)
+    
+    # Store Positions into list
+    verts = []
+    norms = []
+    vcount = len(bm.verts)
+    icount = 3 * len(bm.faces) # Assume triangulated mesh
+    for v in bm.verts:
+        co = scale_mat * rot_x_mat * object_matrix_wrld * v.co
+        norm = scale_mat * rot_x_mat * norm_mat * v.normal
+        verts.append((round(co.x,5), round(co.y,5), round(co.z,5), 1))
+        norms.append(tuple(norm))
+        
+    #Store uvs into List
+    luvs = []
+    #Init at first
+    for v in range(vcount):
+        luvs.append((0.0, 0.0, 0.0, 0.0))
+    
+    #Store uvs this time
+    for f in bm.faces:
+        for l in f.loops:
+            vid = l.vert.index  # Get vertex index
+            # get only the first layer
+            layer = bm.loops.layers.uv[0]
+            u = l[layer].uv.x
+            v = 1. - l[layer].uv.y
+            luvs[vid] = (round(u,5), round(v,5), 0.0, 0.0)
+    
+    # I should add code for the tangent calculations
+    # Leaving it for now
+    
+    vcounter += vcount    # Increasing the offset counter
+    
+    #Storing Indices
+    bm.faces.ensure_lookup_table()
+    faces = []
+    for f in bm.faces:
+        faces.append((icounter + f.verts[0].index,
+                        icounter + f.verts[1].index,
+                        icounter + f.verts[2].index))
+        #indices.append((0,1,2))
+        
+        #print(len(f.verts), vertices[f.verts[0].index],
+        #      vertices[f.verts[1].index],
+        #      vertices[f.verts[2].index])
+        
+        #indices.append(icounter + f.verts[1].index)
+        #indices.append(icounter + f.verts[2].index)
+    
+    #Final Storage
+    vertices.append(verts)
+    normals.append(norms)
+    uvs.append(luvs)
+    indices.append(faces)
+    icounter += icount
 
-#print(vertices)
-#print(indexes)
-#print(uvs)
 
-bpy.ops.object.mode_set(mode='OBJECT')
-
-Create_Data(str(name),
-            'TEST',
-            [str(name)],
-            index_stream = [indexes],
-            vertex_stream = [vertices],
-            uv_stream = [uvs])
+Create_Data('SUZANNE',
+            r"J:\Installs\Steam\steamapps\common\No Man's Sky\GAMEDATA\PCBANKS\TEST",
+            objects,
+            index_stream = indices,
+            vertex_stream = vertices,
+            uv_stream = uvs
+            )
