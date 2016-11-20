921b4e85829982482e15390bb7a5a08fcc3788f6
monkeyman192 <monkey_man_192@yahoo.com.au>
2016-11-21 09:18:57 +1100

    added default values for the TkMaterialData
    this should allow a default material to be created when no material has
    been specified so that every node that is a MESH type has an associated
    material (otherwise the game crashes when trying to load it)

--- classes/Vector4f.py
+++ classes/Vector4f.py
@@ -8,10 +8,10 @@ class Vector4f(Struct):
     def __init__(self, **kwargs):
 
         """ Contents of the struct """
-        self.x = kwargs.get('x', None)
-        self.y = kwargs.get('y', None)
-        self.z = kwargs.get('z', None)
-        self.t = kwargs.get('t', None)
+        self.x = kwargs.get('x', 0.0)
+        self.y = kwargs.get('y', 0.0)
+        self.z = kwargs.get('z', 0.0)
+        self.t = kwargs.get('t', 0.0)
         """ End of the struct contents"""
 
         """ Run code to convert struct contents into self.data_dict """
