79495bc00010d151d90a7fc4cdb4b86885859aaf
monkeyman192 <monkey_man_192@yahoo.com.au>
2017-03-10 16:05:14 +1100

    Updated TkPhysicsComponentData
    Now compatible with Pathfinder update

--- nms_imp/classes/TkPhysicsComponentData.py
+++ nms_imp/classes/TkPhysicsComponentData.py
@@ -11,10 +11,12 @@ class TkPhysicsComponentData(Struct):
 
         """ Contents of the struct """
         self.Data = kwargs.get('Data', TkPhysicsData())
-        self.TriggerVolume = kwargs.get('TriggerVolume', False)
         self.VolumeTriggerType = kwargs.get('VolumeTriggerType', TkVolumeTriggerType())
+		self.SurfaceProperties = kwargs.get('SurfaceProperties', "None")
+		self.TriggerVolume = kwargs.get('TriggerVolume', False)
         self.Climbable = kwargs.get('Climbable', False)
         self.IgnoreModelOwner = kwargs.get('IgnoreModelOwner', False)
+		self.NoVehicleCollision = kwargs.get('NoVehicleCollision', False)
         """ End of the struct contents"""
 
         """ Run code to convert struct contents into self.data_dict """
