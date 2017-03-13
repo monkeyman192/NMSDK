8250dbe692aab4617b2d28a3fcfe776e95f9715d
monkeyman192 <monkey_man_192@yahoo.com.au>
2017-03-13 23:58:13 +1100

    fixed up my stuff up...
    whoops :S

--- nms_imp/classes/TkPhysicsComponentData.py
+++ nms_imp/classes/TkPhysicsComponentData.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 # TkPhysicsComponentData struct
 
 from .Struct import Struct
@@ -26,32 +25,3 @@ class TkPhysicsComponentData(Struct):
         # Parent needed so that it can be a SubElement of something
         self.parent = None
         self.STRUCTNAME = STRUCTNAME
-=======
-# TkPhysicsComponentData struct
-
-from .Struct import Struct
-from .TkPhysicsData import TkPhysicsData
-from .TkVolumeTriggerType import TkVolumeTriggerType
-
-STRUCTNAME = 'TkPhysicsComponentData'
-
-class TkPhysicsComponentData(Struct):
-    def __init__(self, **kwargs):
-
-        """ Contents of the struct """
-        self.Data = kwargs.get('Data', TkPhysicsData())
-        self.VolumeTriggerType = kwargs.get('VolumeTriggerType', TkVolumeTriggerType())
-        self.SurfaceProperties = kwargs.get('SurfaceProperties', "None")
-        self.TriggerVolume = kwargs.get('TriggerVolume', False)
-        self.Climbable = kwargs.get('Climbable', False)
-        self.IgnoreModelOwner = kwargs.get('IgnoreModelOwner', False)
-        self.NoVehicleCollision = kwargs.get('NoVehicleCollision', False)
-        """ End of the struct contents"""
-
-        """ Run code to convert struct contents into self.data_dict """
-        self._create_dict()
-
-        # Parent needed so that it can be a SubElement of something
-        self.parent = None
-        self.STRUCTNAME = STRUCTNAME
->>>>>>> origin/Experimental
