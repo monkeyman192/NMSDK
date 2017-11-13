70810397208f2ab9bd903687f0fe7b801f8268a7
GmrLeon <GmrLeon@users.noreply.github.com>
2017-11-12 23:39:59 -0500

    More Entity Structs. (#24)
    * More Entity Structs.
    
    Went through DestructableComponentData and tried to emulate structure
    observed in ScannableComponentData.
    
    * Fixed GcSubstance Listlessness.
    
    No longer listless!
    
    * Fixed Import Issues
    
    Forgot to import certain structs in GcDestructableComponentData
    
    * Fixing GcSubstanceAmount
    
    Blaah. Interconnected messs.

---
+++ nms_imp/classes/GcSubstanceAmount.py
@@ -0,0 +1,22 @@
+# GcSubstanceAmount struct
+
+from .Struct import Struct
+from .GcRealitySubstanceCategory import GcRealitySubstanceCategory
+
+STRUCTNAME = 'GcSubstanceAmount'
+
+class GcSubstanceAmount(Struct):
+    def __init__(self, **kwargs):
+        super(GcSubstanceAmount, self).__init__()
+
+        """ Contents of the struct """
+        self.data['AmountMin'] = kwargs.get('AmountMin', 0)
+        self.data['AmountMax'] = kwargs.get('AmountMax', 0)
+        self.data['Specific'] = kwargs.get('Specific', "")
+        self.data['SubstanceCategory'] = kwargs.get('SubstanceCategory', GcRealitySubstanceCategory())
+        self.data['Rarity'] = kwargs.get('Rarity', GcRarity())
+        """ End of the struct contents"""
+
+        # Parent needed so that it can be a SubElement of something
+        self.parent = None
+        self.STRUCTNAME = STRUCTNAME
