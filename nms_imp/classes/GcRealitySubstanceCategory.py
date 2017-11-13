dda55bf5f13b904ce28480932fa97ab48faef205
GmrLeon <GmrLeon@users.noreply.github.com>
2017-11-12 22:09:08 -0500

    More Entity Structs.
    Went through DestructableComponentData and tried to emulate structure
    observed in ScannableComponentData.

---
+++ nms_imp/classes/GcRealitySubstanceCategory.py
@@ -0,0 +1,17 @@
+# GcRealitySubstanceCategory struct
+
+from .Struct import Struct
+
+STRUCTNAME = 'GcRealitySubstanceCategory'
+
+class GcRealitySubstanceCategory(Struct):
+    def __init__(self, **kwargs):
+        super(GcRealitySubstanceCategory, self).__init__()
+
+        """ Contents of the struct """
+        self.data['SubstanceCategory'] = kwargs.get('SubstanceCategory', 'None')
+        """ End of the struct contents"""
+
+        # Parent needed so that it can be a SubElement of something
+        self.parent = None
+        self.STRUCTNAME = STRUCTNAME
---
+++ nms_imp/classes/TkTextureResource.py
@@ -0,0 +1,17 @@
+# TkTextureResource struct
+
+from .Struct import Struct
+
+STRUCTNAME = 'TkTextureResource'
+
+class TkTextureResource(Struct):
+    def __init__(self, **kwargs):
+        super(TkTextureResource, self).__init__()
+
+        """ Contents of the struct """
+        self.data['Filename'] = kwargs.get('Filename', "")
+        """ End of the struct contents"""
+
+        # Parent needed so that it can be a SubElement of something
+        self.parent = None
+        self.STRUCTNAME = STRUCTNAME
