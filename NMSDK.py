bc1a1a6494cc1b13095a82a06258907d38e096ca
monkeyman192 <monkey_man_192@yahoo.com.au>
2018-09-21 16:34:01 +1000

    restructured nmsdk.py script to move versioning to new file

--- NMSDK.py
+++ NMSDK.py
@@ -1,24 +1,18 @@
-bl_info = {  
- "name": "No Man's Sky Development Kit",  
- "author": "gregkwaste, monkeyman192",  
- "version": (0, 9),
- "blender": (2, 7, 0),  
- "location": "File > Export",  
- "description": "Create NMS scene structures and export to NMS File format",  
- "warning": "",
- "wiki_url": "",  
- "tracker_url": "",  
- "category": "Export"} 
-
 import bpy
-from BlenderExtensions import *
+from BlenderExtensions import NMSNodes, NMSEntities, NMSPanels
 from addon_script import NMS_Export_Operator
+from plugin_info import PLUGIN_INFO
+
+bl_info = PLUGIN_INFO
 
 customNodes = NMSNodes()
 
+
 # Only needed if you want to add into a dynamic menu
 def menu_func_export(self, context):
-    self.layout.operator(NMS_Export_Operator.bl_idname, text="Export to NMS XML Format ")
+    self.layout.operator(NMS_Export_Operator.bl_idname,
+                         text="Export to NMS XML Format ")
+
 
 def register():
     bpy.utils.register_class(NMS_Export_Operator)
@@ -36,8 +30,9 @@ def unregister():
     NMSEntities.unregister()
 
 
+# TODO: remove
 if __name__ == "__main__":
     register()
 
     # test call
-    bpy.ops.export_mesh.nms(filepath="J:\\Installs\\Steam\\steamapps\\common\\No Man's Sky\\GAMEDATA\\PCBANKS\\CUBE_ODD")
+    bpy.ops.export_mesh.nms(filepath="J:\\Installs\\Steam\\steamapps\\common\\No Man's Sky\\GAMEDATA\\PCBANKS\\CUBE_ODD")  # noqa
---
+++ plugin_info.py
@@ -0,0 +1,11 @@
+PLUGIN_INFO = {
+    "name": "No Man's Sky Development Kit",
+    "author": "gregkwaste, monkeyman192",
+    "version": (0, 9, 1),
+    "blender": (2, 7, 0),
+    "location": "File > Export",
+    "description": "Create NMS scene structures and export to NMS File format",
+    "warning": "",
+    "wiki_url": "https://github.com/monkeyman192/NMSDK/wiki",
+    "tracker_url": "https://github.com/monkeyman192/NMSDK/issues",
+    "category": "Export"}
