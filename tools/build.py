8ad2ae604f9511baf4c6abc7912d60793bcd5024
monkeyman192 <monkey_man_192@yahoo.com.au>
2019-05-09 10:47:58 +1000

    Add automated build script. Updated circleCI to add built zip to artifacts

---
+++ tools/build.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+# Zip the current source files as a blender addon
+
+import os
+import shutil
+import tempfile
+
+COPYFILES = ['BlenderExtensions', 'ModelExporter', 'ModelImporter',
+             'NMS', 'serialization', 'utils', '__init__.py', 'NMSDK.py']
+
+
+nmsdk_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
+zip_dst = os.path.join(nmsdk_path, 'nmsdk')
+
+with tempfile.TemporaryDirectory() as tempdir:
+    nmsdk_src = os.path.join(tempdir, 'nmsdk')
+    os.mkdir(nmsdk_src)
+    for fname in COPYFILES:
+        src = os.path.join(nmsdk_path, fname)
+        dst = os.path.join(nmsdk_src, fname)
+        if os.path.isdir(src):
+            shutil.copytree(src, dst)
+        else:
+            shutil.copy(src, dst)
+    shutil.make_archive(zip_dst, 'zip', nmsdk_src)
