0cd0466203dfcb5d8fc7f26db2f9289c75a185b7
monkeyman192 <monkey_man_192@yahoo.com.au>
2019-05-10 16:58:52 +1000

    attempt to set up automated releases from CircleCI

--- tools/build.py
+++ tools/build.py
@@ -11,11 +11,12 @@ COPYFILES = ['BlenderExtensions', 'ModelExporter', 'ModelImporter',
 
 
 nmsdk_path = os.path.realpath(os.path.join(os.path.dirname(__file__), '..'))
-zip_dst = os.path.join(nmsdk_path, 'nmsdk')
+zip_dst = os.path.join(nmsdk_path, 'build', 'nmsdk')
 
 with tempfile.TemporaryDirectory() as tempdir:
     nmsdk_src = os.path.join(tempdir, 'nmsdk')
     os.mkdir(nmsdk_src)
+    os.mkdir(os.path.join(nmsdk_path, 'build'))
     for fname in COPYFILES:
         src = os.path.join(nmsdk_path, fname)
         dst = os.path.join(nmsdk_src, fname)
