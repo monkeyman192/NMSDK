0c2fb945cbfbfcdba2d2eb96f04f5eb9b43fcbbe
monkeyman192 <monkey_man_192@yahoo.com.au>
2019-05-24 14:25:18 +1000

    [skip ci] Fix issues with version string determination

--- tools/read_nmsdk_version.sh
+++ tools/read_nmsdk_version.sh
@@ -3,14 +3,14 @@
 
 # First, get just the line with the version in it
 ver_line=$(grep 'version' ./__init__.py)
-# THen split it to get just the numbers
-blah=$(echo "$ver_line" | grep -o -E '[0-9]')
+# Then split it to get just the numbers
+numbers=$(echo "$ver_line" | grep -o -E '[0-9]+')
 
 # Function c/o https://stackoverflow.com/a/17841619
 function join_by { local IFS="$1"; shift; echo "$*"; }
 
 # Finally, join the numbers together with a '.'
-ver_str=$(join_by . $blah)
+ver_str=$(join_by . $numbers)
 
 # Return the version
-echo "$ver_str"
+echo "v$ver_str"
