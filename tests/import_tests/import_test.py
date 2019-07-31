64d7891c23bac1de88498f47c77a03fe711b9112
monkeyman192 <monkey_man_192@yahoo.com.au>
2019-07-31 23:27:36 +1000

    improve import test and begin writing test to check for errors on import

--- tests/import_tests/import_test.py
+++ tests/import_tests/import_test.py
@@ -1,15 +1,33 @@
 import subprocess
+import os
+import os.path as op
+import pytest
 
 
-CRYSTAL_TEST_PATH = "C:\\Program Files\\Blender Foundation\\Blender\\2.79\\scripts\\addons\\nmsdk\\tests\\import_tests\\import_crystal.py"  # noqa
+PLUGIN_PATH = os.getcwd()
+TESTS_PATH = 'tests\\import_tests'
+
+CRYSTAL_TEST_PATH = op.join(PLUGIN_PATH, TESTS_PATH, 'import_crystal.py')
+BAD_IMPORT_TEST_PATH = op.join(PLUGIN_PATH, TESTS_PATH, 'import_bad.py')
 
 
-def test_import_crystal():
+def run_test(test_path):
     proc = subprocess.Popen(['blender', '-b', '-noaudio',
-                             '--python', CRYSTAL_TEST_PATH],
+                             '--python', test_path],
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE)
     # Capture the stderr
     _, stderr = proc.communicate()
     # Ensure that there were no errors
     assert stderr.decode() == ''
+
+
+def test_import_crystal():
+    # Test the importing of the crystal model
+    run_test(CRYSTAL_TEST_PATH)
+
+
+@pytest.mark.skip(reason="Test doesn't work yet... :(")
+def test_import_bad():
+    # Test the importing of data that is bad and should raise errors
+    run_test(BAD_IMPORT_TEST_PATH)
